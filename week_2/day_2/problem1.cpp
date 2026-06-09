f555f8c466eede78575bb83976fe660ac969434f
Anushka Singh <24110cn054@glbitm.ac.in>
2026-06-09 18:34:31 +0530

    Update problem1.cpp

--- week_2/day_2/problem1.cpp
+++ week_2/day_2/problem1.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+int lengthOfLongestSubstring(string s) {
+        unordered_set<char> ust;
 
+        int left = 0;
+        int sol = 0;
+
+        for (int right = 0; right < s.size(); right++)
+         {
+        while (ust.count(s[right])) 
+        {
+                ust.erase(s[left]);
+                left++;
+            }
+
+            ust.insert(s[right]);
+            sol = max(sol, right - left + 1);
+            
+        }
+
+        return sol;
+    }
+};
