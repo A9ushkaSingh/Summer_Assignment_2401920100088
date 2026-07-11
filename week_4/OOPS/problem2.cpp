cc081e9177495a095932e2ad2d3f635ab4152ce4
Anushka Singh <24110cn054@glbitm.ac.in>
2026-07-11 09:40:01 +0530

    Create problem2.cpp

---
+++ week_4/OOPS/problem2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Outer
+{
+public:
+    void display()
+    {
+        cout << "Display of Outer Class" << endl;
+    }
+
+    class Inner
+    {
+    public:
+        void display()
+        {
+            cout << "Display of Inner Class" << endl;
+        }
+    };
+};
+
+int main()
+{
+    Outer o;
+    Outer::Inner i;
+
+    o.display();
+    i.display();
+
+    return 0;
+}
